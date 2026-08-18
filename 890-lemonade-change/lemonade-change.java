404c080891bee3300951e12023dc0cf584db255c
Rishikafulwani177 <fulwanirishika@gmail.com>
2026-08-18 21:31:41 +0530

    Time: 2 ms (97.98%) | Memory: 72.1 MB (94.11%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        for(int i=0; i<bills.length; i++){
+            if(bills[i]==5){
+                five++;
+            } else if(bills[i]==10){
+                ten++;
+                five--;
+                if(five<0){
+                    return false;
+                }
+            } else {
+                if(ten>=1 && five >=1){
+                    ten--;
+                    five--;
+                } else if(five >= 3){
+                    five=five-3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
