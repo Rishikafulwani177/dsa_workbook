6fada8fe17545fa043d3fc8bb80fdf9a9ab1f24e
Rishikafulwani177 <fulwanirishika@gmail.com>
2026-08-29 04:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (47.15%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,12 +1,9 @@
 class Solution {
     public int hammingWeight(int n) {
-        int res=0; 
+        int res=0;
         while(n>0){
-            int bit= n%2;
-            if(bit==1){
-                res++;
-            }
-            n=n/2;
+            res++;
+            n= n & (n-1);
         }
         return res;
     }
