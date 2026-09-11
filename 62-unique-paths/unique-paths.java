f751b624918ba368361efb6c933eada24dff2e20
Rishikafulwani177 <fulwanirishika@gmail.com>
2026-09-11 23:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (28.48%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,30 +1,27 @@
 class Solution {
+    public int fun(int i, int j, int m, int n, int[][] dp){
+        if(i<0 || j<0 || i>=m || j>=n){
+            return 0;
+        }
+
+        if( i==m-1 && j== n-1){
+            return 1;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int c1= fun(i+1, j, m, n, dp);
+        int c2= fun(i, j+1, m, n, dp);
+        return dp[i][j]= c1+c2;
+    }
+
     public int uniquePaths(int m, int n) {
         int[][] dp= new int[m+1][n+1];
-        // Last row = 1
-        // From any cell in the last row,
-        // there is only one way to reach the destination:
-        // move right.
-        for (int j = 0; j < n; j++) {
-            dp[m - 1][j] = 1;
+        for(int i=0; i<dp.length; i++){
+            Arrays.fill(dp[i],-1);
         }
-
-        // Last column = 1
-        // From any cell in the last column,
-        // there is only one way to reach the destination:
-        // move down.
-        for (int i = 0; i < m; i++) {
-            dp[i][n - 1] = 1;
-        }
-
-        for(int i=m-2; i>=0; i--){
-            for(int j=n-2; j>=0; j--){
-                int c1=dp[i+1][j];
-                int c2=dp[i][j+1];
-                dp[i][j]=c1+c2;
-            }
-        }
-
-        return dp[0][0];
+        return fun(0,0,m,n,dp);
     }
 }
